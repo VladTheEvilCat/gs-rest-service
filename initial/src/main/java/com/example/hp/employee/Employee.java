ff3f07b5a8ae4be9df8da9cdc679b1adb08ab00d
Vlad The Evil Cat <vjcaso@gmail.com>
2025-02-21 14:55:02 -0500

    Added employee stuff

---
+++ initial/src/main/java/com/example/hp/employee/Employee.java
@@ -0,0 +1,70 @@
+package com.example.hp.employee;
+
+public class Employee {
+
+    private String employee_id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String title;
+
+    public Employee(){}
+    public Employee(String employee_id, String first_name, String last_name, String email, String title) {
+        this.employee_id = employee_id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.title = title;
+    }
+
+    public String getEmployee_id() {
+        return this.employee_id;
+    }
+
+    public void setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public String getFirst_name() {
+        return this.first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return this.last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employee_id='" + this.employee_id + '\'' +
+                ", first_name='" + this.first_name + '\'' +
+                ", last_name='" + this.last_name + '\'' +
+                ", email='" + this.email + '\'' +
+                ", title='" + this.title + '\'' +
+                '}';
+    }
+}
